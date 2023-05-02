df0a3ef287bacc422b5ec073c32b283c9c87a31d
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-05-02 09:04:10 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h2 className="text-4xl">Hello world</h2>
+  </React.StrictMode>
+);

979400af32cb5576ba48e810fcf1852bbe876816
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-05-02 14:59:36 +0600

    added star

--- src/components/Home/Slider/Slider.jsx
+++ src/components/Home/Slider/Slider.jsx
@@ -1,3 +1,4 @@
+import { Rating } from "flowbite-react";
 import React from "react";
 
 const Slider = ({ data }) => {
@@ -10,7 +11,14 @@ const Slider = ({ data }) => {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${image})`,
       }}
     >
-      <div className="text-white grid items-center justify-center lg:pt-52 text-center space-y-4">
+      <div className="text-white grid items-center justify-center lg:pt-48 text-center space-y-4">
+        <Rating size="md" className="mx-auto">
+          <Rating.Star className="text-slate-50" />
+          <Rating.Star className="text-slate-50" />
+          <Rating.Star className="text-slate-50" />
+          <Rating.Star className="text-slate-50" />
+          <Rating.Star className="text-slate-50" />
+        </Rating>
         <h1 className="text-5xl font-bold">{title}</h1>
         <p className="text-xl ">{desc}</p>
       </div>

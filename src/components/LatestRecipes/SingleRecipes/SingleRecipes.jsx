82b55da4980e494ef0862654b114ff135d0e64e0
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-05-05 21:09:52 +0600

    Project is fullay compleated. (Last Update : Modify all Problem and solve some ishu and added Lazy loadind and come Alert is customize)

--- src/components/LatestRecipes/SingleRecipes/SingleRecipes.jsx
+++ src/components/LatestRecipes/SingleRecipes/SingleRecipes.jsx
@@ -1,4 +1,4 @@
-import { Card } from "flowbite-react";
+// import { Card } from "flowbite-react";
 import React from "react";
 import { FaRegClock, FaRegFolderOpen } from "react-icons/fa";
 
@@ -6,12 +6,23 @@ const SingleRecipes = ({ recipe }) => {
   const { title, desc, image, type, time } = recipe;
 
   return (
-    <div className="max-w-sm pb-10">
-      <Card imgAlt="this is image" imgSrc={image}>
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md mb-10">
+      <img
+        className="rounded-t-lg"
+        src={image}
+        alt="cover image"
+        loading="lazy"
+        width={390}
+        height={500}
+      />
+
+      <div className="p-6">
         <h5 className="text-2xl font-bold tracking-tight text-slate-600 dark:text-white  hover:text-orange-500 duration-300">
           {title}
         </h5>
-        <p className="font-normal text-gray-700  dark:text-gray-400">{desc}</p>
+        <p className="font-normal text-gray-700  dark:text-gray-400 py-4">
+          {desc}
+        </p>
         <div className="  flex items-center justify-around py-3 font-semibold  text-slate-600 mt-2">
           <div className=" hover:text-orange-500 p-2  flex items-center gap-2">
             <FaRegFolderOpen />
@@ -22,7 +33,7 @@ const SingleRecipes = ({ recipe }) => {
             <p>{time}</p>
           </div>
         </div>
-      </Card>
+      </div>
     </div>
   );
 };
--- src/pages/SavedRecipes/SavedRecipes.jsx
+++ src/pages/SavedRecipes/SavedRecipes.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 import { Button } from "flowbite-react";
-import { getShoppingCart } from "../../utils/localStorage";
+import { useEffect, useState } from "react";
+import { deleteShoppingCart, getShoppingCart } from "../../utils/localStorage";
 const SavedRecipes = () => {
-  const showData = getShoppingCart();
+  const [recipes, setRecipes] = useState("");
 
-  const recipes = Object.keys(showData);
-  const removeStorage = () => {};
+  useEffect(() => {
+    const showData = getShoppingCart();
+    setRecipes(Object.keys(showData));
+  }, []);
+  const removeStorage = () => {
+    deleteShoppingCart();
+    setRecipes("");
+  };
+
   if (recipes.length <= 0) {
     return (
       <h1 className="containeR h-[400px] w-full grid items-center justify-center text-2xl text-orange-500 border border-orange-500 rounded-lg my-2">
@@ -16,26 +24,32 @@ const SavedRecipes = () => {
   }
 
   return (
-    <div className="containeR grid grid-cols-1 md:grid-cols-3  gap-3 items-center justify-center text-orange-500  rounded-lg ">
-      {recipes.map((rec, indx) => {
-        return (
-          <div
-            key={indx}
-            className="border border-orange-500 p-3 text-justify rounded-md py-4 flex items-center justify-between"
-          >
-            <h1 className="text-xl font-mono text-orange-500 font-bold py-2">
-              {rec}
-            </h1>
-            <Button
-              pill={true}
-              gradientDuoTone="pinkToOrange"
-              onClick={removeStorage}
+    <div className="containeR">
+      <div className="flex justify-around items-center my-4">
+        <h1 className="text-3xl font-semibold font-sans text-slate-600">
+          All Favorites Recipes Are Hear..{" "}
+        </h1>
+        <Button gradientDuoTone="pinkToOrange" onClick={removeStorage}>
+          Clear All Favorite
+        </Button>
+      </div>
+      <div className=" grid grid-cols-1 md:grid-cols-3  gap-3 items-center justify-center text-orange-500  rounded-lg ">
+        {recipes.map((rec, indx) => {
+          return (
+            <div
+              key={indx}
+              className="border border-orange-500 p-3 text-justify rounded-md py-4 flex items-center justify-between"
             >
-              Remove
-            </Button>
-          </div>
-        );
-      })}
+              <h1 className="text-xl font-mono text-orange-500 font-bold py-2">
+                {rec}
+              </h1>
+              <Button pill={true} gradientDuoTone="pinkToOrange">
+                Remove
+              </Button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
